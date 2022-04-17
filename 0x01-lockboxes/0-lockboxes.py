55e68f46107293c931a5183758baf0ddc8878414
OBigVee <adedejivictor98@gmail.com>
2022-04-17 18:50:02 +0100

    Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# def canUnlockAll(boxes):
+#     checker = False
+#     for boxList in boxes:
+#         print(boxList)
+#         for boxItem in boxList:
+#             print(boxes.index(boxes[boxList][boxItem]))
+#             print(boxItem)
+#             if (boxList[boxItem] == boxes.index(boxes[boxList][boxItem])):
+#                 return True
+#             else:
+#                 return False
+
+"""Defines a function that determines if a box containing a list
+   of lists can be opened using keys stored in the lists
+"""
+
+
+def canUnlockAll(boxes):
+    """Determines if boxes can be trackUnlockedBox"""
+    index = 0
+    trackUnlockedBox = {}
+
+    for box in boxes:
+        if len(box) == 0 or index == 0:
+            trackUnlockedBox[index] = "opened"
+        for key in box:
+            if key < len(boxes) and key != index:
+                trackUnlockedBox[key] = key
+        if len(trackUnlockedBox) == len(boxes):
+            return True
+        index += 1
+    return False
