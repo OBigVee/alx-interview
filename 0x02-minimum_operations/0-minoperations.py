07ace3e25f042490c79e06191cefd93d088a5630
OBigVee <adedejivictor98@gmail.com>
2022-04-24 22:56:16 +0100

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 import time
+
 """Minimum Operations"""
 
-def isEven(n: int) -> int:
+
+def isEven(n: int) -> bool:
     """Even number checker, returns boolean"""
     if n % 2 == 0:
         checker = True
@@ -10,32 +12,45 @@ def isEven(n: int) -> int:
         checker = False
     return checker
 
+
+# def minOperations(n: int) -> int:
+#     """returns minimum number of operation"""
+#     # print("check the value of n: n = {}".format(str(n)))
+#     trackOps: int = 0
+#     leastOps: int = 2
+#     # s = time.perf_counter()
+#     # print("Computation started in ----- {} time -------".format(str(s)))
+#     while n > 1:
+#         while n % leastOps == 0:
+#             # while isEven(n):
+#             #     if n==1:
+#             #         continue
+#             trackOps += leastOps
+#             n //= leastOps
+#             # else:
+#             # #if  isEven(n):
+#             #     trackOps += leastOps
+#             #     n //= leastOps
+#             # print("n={} div by leastOps={}:: Equals {}".format(str(n),str(leastOps),str(n)))
+#         # else:
+#         # #     continue
+#         #     print("n == {}.".format(str(n)))
+#         #     time.sleep(10)
+#         # print("end Inner While LOOP -> back to Outter LOOP")
+#         leastOps += 1
+#     # elapsed = time.perf_counter() - s
+#     # print("minOpstakes ------- {} time to compute -------".format(str(elapsed)))
+#     # print(trackOps)
+#     return trackOps
+
+
 def minOperations(n: int) -> int:
     """returns minimum number of operation"""
-    # print("check the value of n: n = {}".format(str(n)))
     trackOps: int = 0
     leastOps: int = 2
-    # s = time.perf_counter()
-    # print("Computation started in ----- {} time -------".format(str(s)))
     while n > 1:
         while n % leastOps == 0:
-            # while isEven(n):
-            #     if n==1:
-            #         continue
             trackOps += leastOps
             n //= leastOps
-            # else:
-            # #if  isEven(n):
-            #     trackOps += leastOps
-            #     n //= leastOps
-            # print("n={} div by leastOps={}:: Equals {}".format(str(n),str(leastOps),str(n)))
-        # else:
-        # #     continue
-        #     print("n == {}.".format(str(n)))
-        #     time.sleep(10)
-        # print("end Inner While LOOP -> back to Outter LOOP")
         leastOps += 1
-    # elapsed = time.perf_counter() - s
-    # print("minOpstakes ------- {} time to compute -------".format(str(elapsed)))
-    # print(trackOps)
     return trackOps
