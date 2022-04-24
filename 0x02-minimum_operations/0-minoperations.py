c4532c358658949fa35f9f2abf6b87089b46cf48
OBigVee <adedejivictor98@gmail.com>
2022-04-24 22:48:43 +0100

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import time
 
+
 def isEven(n: int) -> int:
     """Even number checker, returns boolean"""
     if n % 2 == 0:
@@ -8,26 +9,34 @@ def isEven(n: int) -> int:
     else:
         checker = False
     return checker
-    
 
 
 def minOperations(n: int) -> int:
     """returns minimum number of operation"""
-    print("check the value of n: n = {}".format(str(n)))
+    # print("check the value of n: n = {}".format(str(n)))
     trackOps: int = 0
     leastOps: int = 2
-    #s = time.perf_counter()
-    #print("Computation started in ----- {} time -------".format(str(s)))
+    # s = time.perf_counter()
+    # print("Computation started in ----- {} time -------".format(str(s)))
     while n > 1:
-        #while n % leastOps == 0:
-        if  isEven(n):
+        while n % leastOps == 0:
+            # while isEven(n):
+            #     if n==1:
+            #         continue
             trackOps += leastOps
             n //= leastOps
-            #print("n={} div by leastOps={}:: Equals {}".format(str(n),str(leastOps),str(n)))
+            # else:
+            # #if  isEven(n):
+            #     trackOps += leastOps
+            #     n //= leastOps
+            # print("n={} div by leastOps={}:: Equals {}".format(str(n),str(leastOps),str(n)))
         # else:
+        # #     continue
         #     print("n == {}.".format(str(n)))
         #     time.sleep(10)
+        # print("end Inner While LOOP -> back to Outter LOOP")
         leastOps += 1
-    #elapsed = time.perf_counter() - s
-    #print("minOpstakes ------- {} time to compute -------".format(str(elapsed)))
+    # elapsed = time.perf_counter() - s
+    # print("minOpstakes ------- {} time to compute -------".format(str(elapsed)))
+    # print(trackOps)
     return trackOps
