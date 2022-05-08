37196f0fba9bc17eeaa069ed85dc5d42c81926e8
OBigVee <adedejivictor98@gmail.com>
2022-05-09 00:44:34 +0100

    UTF-8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""UTF-8 validator"""
+
+
+def validUTF8(data):
+    """
+        """
+
+    data = iter(data)
+    for leading_byte in data:
+        leading_ones = _count_leading_ones(leading_byte)
+        if leading_ones in [1, 7, 8]:
+            return False
+        for _ in range(leading_ones - 1):
+            trailing_byte = next(data, None)
+            if trailing_byte is None or trailing_byte >> 6 != 0b10:
+                return False
+    return True
+
+
+def _count_leading_ones(byte):
+    """Counts the leading ones."""
+
+    for i in range(8):
+        if byte >> 7 - i == 0b11111111 >> 7 - i & ~1:
+            return i
