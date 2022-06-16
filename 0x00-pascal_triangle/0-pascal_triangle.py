ed3656c3cdcf9304c6413470a1381d2b24720bce
OBigVee <adedejivictor98@gmail.com>
2022-06-16 10:26:27 +0100

    :white_check_mark:

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,47 +1,23 @@
 #!/usr/bin/python3
 
 """function returns a list of lists of integers representing the
-Pascal's triangle of n"""
+Pascal's triangle of any given integer n"""
 
 
-from logging import raiseExceptions
 
-
-def factorial(n):
-    """Function computes the factorial of ank number"""
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return n * factorial(n - 1)
-
-
-def combination(n, k):
-    return factorial(n) // (factorial(n - k) * factorial(k))
-
-# def pascal_triangle(n):
-#     num = [[1]]
-#     compute_n =[]
-#     if not isinstance(n,int):
-#         raiseExceptions("n is not an int")
-#     elif (n < 0):
-#         return num
-#     else:
-#         for i in range(n+1):
-#             compute_ = ""
-#             for j in range(i-1):
-#                 #compute_ =""
-#                 compute_ = [combination(i,j) for j in range(i+1) ]
-#            #print(compute_n,end = " ".format(int(compute_n)))
-#             # for j in range(i+1):
-#             #     compute_n = list(combination(i,j))
-#             num.append(compute_)
-#     return num
-    
 def pascal_triangle(n):
-    assert type(n) == int
-    if n <= 0:
-        return []
-    return [[1]] + [
-        [combination(n, k) for k in range(n+1)]
-        for n in range(1, n)
-    ]
+    '''returns a list of lists of integers representing
+    the Pascal's triangle of any given integer n.
+    '''
+    triangle = []
+    if type(n) is not int or n <= 0:
+        return triangle
+    for i in range(n):
+        line = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                line.append(1)
+            elif i > 0 and j > 0:
+                line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        triangle.append(line)
+    return triangle
