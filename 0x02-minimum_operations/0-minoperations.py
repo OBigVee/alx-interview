e322e22606787c7960bc6688ef63269076891b28
OBigVee <adedejivictor98@gmail.com>
2022-06-16 10:35:38 +0100

    :white_check_mark: Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,54 +1,28 @@
 #!/usr/bin/python3
-import time
 """Minimum Operations"""
 
 
-def isEven(n: int) -> bool:
-    """Even number checker, returns boolean"""
-    if n % 2 == 0:
-        checker = True
-    else:
-        checker = False
-    return checker
+def minOperations(n):
+    '''Computes the fewest number of operations needed to result
+    in exactly n H characters.
+    '''
+    if not isinstance(n, int):
+        return 0
+    trackOPerations = 0
+    clipboard = 0
+    done = 1
 
-# def minOperations(n: int) -> int:
-#     """returns minimum number of operation"""
-#     # print("check the value of n: n = {}".format(str(n)))
-#     trackOps: int = 0
-#     leastOps: int = 2
-#     # s = time.perf_counter()
-#     # print("Computation started in ----- {} time -------".format(str(s)))
-#     while n > 1:
-#         while n % leastOps == 0:
-#             # while isEven(n):
-#             #     if n==1:
-#             #         continue
-#             trackOps += leastOps
-#             n //= leastOps
-#             # else:
-#             # #if  isEven(n):
-#             #     trackOps += leastOps
-#             #     n //= leastOps
-#             # print("n={} div by leastOps={}:: Equals {}".format(str(n),str(leastOps),str(n)))
-#         # else:
-#         # #     continue
-#         #     print("n == {}.".format(str(n)))
-#         #     time.sleep(10)
-#         # print("end Inner While LOOP -> back to Outter LOOP")
-#         leastOps += 1
-#     # elapsed = time.perf_counter() - s
-#     # print("minOpstakes ------- {} time to compute -------".format(str(elapsed)))
-#     # print(trackOps)
-#     return trackOps
+    while done < n:
+        if clipboard == 0:
+            clipboard = done
+            done += clipboard
+            trackOPerations += 2
+        elif n - done > 0 and (n - done) % done == 0:
+            clipboard = done
+            done += clipboard
+            trackOPerations += 2
+        elif clipboard > 0:
+            done += clipboard
+            trackOPerations += 1
 
-
-def minOperations(n: int) -> int:
-    """returns minimum number of operation"""
-    trackOps: int = 0
-    leastOps: int = 2
-    while n > 1:
-        while n % leastOps == 0:
-            trackOps += leastOps
-            n //= leastOps
-        leastOps += 1
-    return trackOps
+    return trackOPerations
