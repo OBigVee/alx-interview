2c4bc59b1cd3a25b5713469c2d229a96215ed91f
OBigVee <adedejivictor98@gmail.com>
2022-06-16 10:28:25 +0100

    :white_check_mark:

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-
 """function returns a list of lists of integers representing the
-Pascal's triangle of any given integer n"""
-
+Pascal's triangle of any given integer n
+"""
 
 
 def pascal_triangle(n):
-    '''returns a list of lists of integers representing
+    """returns a list of lists of integers representing
     the Pascal's triangle of any given integer n.
-    '''
+    """
     triangle = []
     if type(n) is not int or n <= 0:
         return triangle
