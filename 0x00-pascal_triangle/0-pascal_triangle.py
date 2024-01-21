88b9286df1c2d3aa3f18446f8d75b051620aa3a1
obigvee <adedejivictor98@gmail.com>
2024-01-21 15:07:37 +0100

    :heavy_check_mark:

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+pascal's triangle
+function returns a list of lists of integers representing
+the Pascal's triangle of n:
+"""
+def _combination(n, r):
+    """Compute the combination of two variables"""
+    from functools import reduce
+
+    def n_fac(n):
+        """Find the factorial of an integer"""
+        fac = 1
+        for x in range(1, n+1):
+            fac *= x
+        return fac
+    return n_fac(n) // (n_fac(n - r) * n_fac(r))
+
+
+def pascal_triangle(n):
+    """Generates the pascal triangle for a power, excluding (x+y)^power"""
+    assert type(n) == int
+    if n <= 0:
+        return []
+
+    return [[1]] + [
+        [_combination(x, y) for y in range(x+1)]
+        for x in range(1, n)
