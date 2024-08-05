7e9cc58aa249d9df4f3190d104da80f6a7ec6c8f
obigvee <adedejivictor98@gmail.com>
2024-08-05 08:33:32 +0100

    :tada:

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,7 +8,12 @@ def island_perimeter(grid) -> int:
     visit = set()
 
     def dfs(i, j):
-        if i >= len(grid) or j >= len(grid[0]) or i < 0 or j < 0 or grid[i][j] == 0:
+        if (i >= len(grid) or
+                j >= len(grid[0]) or
+                i < 0 or
+                j < 0 or
+                grid[i][j] == 0):
+
             return 1
         if (i, j) in visit:
             return 0
@@ -18,9 +23,8 @@ def island_perimeter(grid) -> int:
         perimeter += dfs(i, j - 1)
         perimeter += dfs(i - 1, j)
         return perimeter
+
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j]:
                 return dfs(i, j)
-            
-            #return ""
